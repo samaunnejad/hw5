862dc783e703847c6b2aa92f2a2d3192f998514c
Samaun Nejad <snejad@usc.edu>
2022-12-04 05:03:12 +0000

    All done

--- schedwork.cpp
+++ schedwork.cpp
@@ -19,8 +19,7 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
-
+bool helper(const AvailabilityMatrix&, const size_t, const size_t, DailySchedule&, int, int, int, vector<int>);
 // Add your implementation of schedule() and other helper functions here
 
 bool schedule(
@@ -35,9 +34,46 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    vector<int> shiftsByWorker;
+    for(int i = 0;i<(int)avail[0].size();i++) shiftsByWorker.push_back(0);
+    for(int i = 0;i<(int)avail.size();i++) sched.push_back(vector<unsigned int>());
 
-
-
-
+    return helper(avail, dailyNeed, maxShifts, sched, 0, 0, 0, shiftsByWorker);
 }
 
+
+
+
+
+
+
+
+
+bool helper(const AvailabilityMatrix& avail,const size_t dailyNeed,const size_t maxShifts,DailySchedule& sched, int day, int shiftNum, int worker, vector<int> shiftsByWorker)
+{
+    if(day == (int)avail.size()){
+        return true;
+    }
+    else if(shiftNum == (int)dailyNeed){
+        cout << "next day" << endl;
+        return helper(avail,dailyNeed,maxShifts,sched,day+1,0,0,shiftsByWorker);
+    }
+    else{
+        for(int i = worker;i<(int)avail[day].size();i++){
+            cout << i << endl;
+            if(avail[day][i] && (shiftsByWorker[i] < (int)maxShifts)){
+                cout << "viable option" << endl;
+                sched[day].push_back(i);
+                shiftsByWorker[i]++;
+                cout << "recursive call" << endl;
+                if(helper(avail,dailyNeed,maxShifts,sched,day,shiftNum+1,i+1,shiftsByWorker)){
+                    return true;
+                }
+                //remove most recent addition
+                sched[day].pop_back();
+                shiftsByWorker[i]--;
+            }
+        }
+        return false;
+    }
+}
--- wordle.cpp
+++ wordle.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void helper(std::string, std::string, std::set<std::string>,std::set<std::string>&);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -18,8 +18,56 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
-
+    std::set<std::string> toReturn;
+    helper(in,floating,dict,toReturn);
+    return toReturn;
 }
 
 // Define any helper functions here
+
+void helper(std::string in, std::string floating, std::set<std::string> dict, std::set<std::string>& toReturn){
+    if(in == "") return;
+    int firstDash = in.find("-");
+    if(firstDash == std::string::npos){
+        if(dict.find(in) != dict.end()){
+            toReturn.insert(in);
+        }
+    }
+    else{
+        if(floating.length() == 0){
+            for(int i = 'a';i <= 'z';i++){
+                string temp = in;
+                temp[firstDash] = (char)i;
+                helper(temp,floating, dict,toReturn);
+            }
+        }
+        else{
+            int numDashes = 0;
+            for(int i = 0;i<(int)in.length();i++){
+                if(in[i] == '-'){
+                    numDashes++;
+                } 
+            }
+            if(numDashes == (int)floating.length()){
+                for(int i = 0;i<(int)floating.length();i++){
+                    string temp = in;
+                    temp[firstDash] = floating[i];
+                    string tempFloating = floating.substr(0,i) + floating.substr(i+1);
+                    helper(temp,tempFloating, dict,toReturn);
+                }
+            }
+            else{
+                for(int i = 'a';i <= 'z';i++){
+                    int floatingIndex = floating.find((char)i);
+                    string tempFloating = floating;
+                    if(floatingIndex != std::string::npos){
+                        tempFloating = floating.substr(0,floatingIndex) + floating.substr(floatingIndex+1);
+                    }
+                    string temp = in;
+                    temp[firstDash] = (char)i;
+                    helper(temp,tempFloating, dict,toReturn);
+                }
+            }
+        }
+    }
+}
